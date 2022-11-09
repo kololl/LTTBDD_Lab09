a3a1a31c35cf67790a3047451658dd19ba1e9425
PhuongThao15 <tieuhivong7201@gmail.com>
2022-11-09 18:05:20 +0700

    bài 2

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import bai01 from "./screens/bai01";
+import bai02 from "./screens/bai02";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="bai01" component={bai01} />
+        {/* <Stack.Screen name="bai02" component={bai02} /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
